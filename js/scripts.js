f5141ba15c13f9b51227b31e83cfb079086fbd09
Molly Curtin <patternandshape@gmail.com>
2016-05-14 14:09:54 -0700

    add fade in and out function to each project from the main web design page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+
   $(".vc").click(function() {
+    $(".project-gallery").fadeOut();
+    
     $.when($(".project-gallery").fadeOut(1000)).then(function() {
       $(".visible-cloaks").fadeIn(1000);
     });
   });
+  event.preventDefault();
+});
