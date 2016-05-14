62f7b316f5c18a2aa363dcd2c266dc2467c96866
Molly Curtin <patternandshape@gmail.com>
2016-05-14 01:40:14 -0700

    add attempt of fade-in and out of divs, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
-// $(document).ready(function(){
-//
-// });
-
-
-// changes page layout on form submission and ideally fades the form out as well
-
- // $.when($(".formBox").fadeOut(1000)).then(function() {
- //   $(".list").fadeIn(1000);
- //   $(".col-md-4.second").addClass("col-md-8").removeClass("col-md-4");
- //
- // });
-
- // $('#btn').click(function(e){
- //     $('#fancy, #btn').fadeOut('slow', function(){
- //         $('#bank, #btn-bk').fadeIn('slow');
- //     });
- // });
- //
- //     $('#btn-bk').click(function(e){
- //         $('#bank, #btn-bk').fadeOut('slow', function(){
- //             $('#fancy, #btn').fadeIn('slow');
- //         });
- //     });
+$(document).ready(function() {
+  $(".vc").click(function() {
+    $.when($(".project-gallery").fadeOut(1000)).then(function() {
+      $(".visible-cloaks").fadeIn(1000);
+    });
+  });
