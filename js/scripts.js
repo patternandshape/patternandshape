08e0361940a9ac71422c61a4fe4ae7d3ebbcac15
Molly Curtin <patternandshape@gmail.com>
2016-05-18 15:48:27 -0700

    add something that is happening

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,88 @@
 $(document).ready(function() {
+  $('.gallery-item').click(function() {
+    $('.project-gallery').fadeOut("500");
 
-  $(".vc").click(function() {
-    $(".project-gallery").fadeOut("500");
+    $gallery_item = jQuery(this);
+    var gallery_item_id = parseInt($gallery_item.attr('id').replace('.gallery-item', ''), 10);
 
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".visible-cloaks").fadeIn("500");
+    var $projects = $('.projects');
+    $projects.each(function() {
+
+      $div = jQuery(this);
+      var div_id = parseInt($div.attr('id').replace('project', ''), 10);
+
+      $div.fadeOut("500", function() {
+        if (div_id == gallery_item_id) {
+          $div.fadeIn("500");
+        }
+      });
     });
   });
+  $('.list').click(function() {
+    $list = jQuery(this);
+    var list_id = parseInt($list.attr('id').replace('list', ''), 10);
 
-  $(".boomb").click(function() {
-    $(".project-gallery").fadeOut("500");
+    var $projects = $('.projects');
+    $projects.each(function() {
 
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".boom-bap").fadeIn("500");
-    });
-  });
+      $div = jQuery(this);
+      var div_id = parseInt($div.attr('id').replace('project', ''), 10);
 
-  $(".eames-o").click(function() {
-    $(".project-gallery").fadeOut("500");
-
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".eames-office").fadeIn("500");
-    });
-  });
-
-  $(".fw").click(function() {
-    $(".project-gallery").fadeOut("500");
-
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".folkways").fadeIn("500");
-    });
-  });
-
-  $(".mo").click(function() {
-    $(".project-gallery").fadeOut("500");
-
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".mosk").fadeIn("500");
-    });
-  });
-
-  $(".crate-link").click(function() {
-    $(".project-gallery").fadeOut("500");
-
-    $.when($(".project-gallery").fadeOut("500")).then(function() {
-      $(".crate").fadeIn("500");
+      $div.fadeOut("500", function() {
+        if (div_id == list_id) {
+          $div.fadeIn("500");
+        }
+      });
     });
   });
 
   event.preventDefault();
 });
+
+// $(".vc").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".visible-cloaks").fadeIn("500");
+//   });
+// });
+//
+// $(".boomb").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".boom-bap").fadeIn("500");
+//   });
+// });
+//
+// $(".eames-o").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".eames-office").fadeIn("500");
+//   });
+// });
+//
+// $(".fw").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".folkways").fadeIn("500");
+//   });
+// });
+//
+// $(".mo").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".mosk").fadeIn("500");
+//   });
+// });
+//
+// $(".crate-link").click(function() {
+//   $(".project-gallery").fadeOut("500");
+//
+//   $.when($(".project-gallery").fadeOut("500")).then(function() {
+//     $(".crate").fadeIn("500");
+//   });
+// });
