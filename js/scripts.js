bbbd928346d10ed169fd3f5d1ed5aeb156dfe510
Molly Curtin <patternandshape@gmail.com>
2016-05-19 15:54:09 -0700

    add top photo div styling on project pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.gallery-item').click(function() {
     $('.project-gallery').fadeOut(250);
+
     $gallery_item = jQuery(this);
 
     var gallery_item_id = parseInt($gallery_item.attr('id').replace('gallery', ''), 10);
@@ -11,15 +12,16 @@ $(document).ready(function() {
       $div = jQuery(this);
       var div_id = parseInt($div.attr('id').replace('project', ''), 10);
 
-      $div.fadeOut("500", function() {
-        if (div_id == gallery_item_id) {
-          $div.fadeIn("500");
+      $div.fadeOut(500, function() {
+        if (div_id === gallery_item_id) {
+          $div.fadeIn(500);
         }
       });
     });
   });
+
   $('.list').click(function() {
-    $('.project-gallery').fadeOut(250);
+    $('.project-gallery').fadeOut(500);
     $list = jQuery(this);
     var list_id = parseInt($list.attr('id').replace('list', ''), 10);
 
@@ -29,7 +31,7 @@ $(document).ready(function() {
       $div = jQuery(this);
       var div_id = parseInt($div.attr('id').replace('project', ''), 10);
 
-      $div.fadeOut("500", function() {
+      $div.fadeOut(500, function() {
         if (div_id == list_id) {
           $div.fadeIn(500);
         }
